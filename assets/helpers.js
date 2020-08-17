27d07a8a63d0093931ff4dddba00db030ef52c68
devincheca <devincheca@gmail.com>
2020-08-16 20:53:12 -0700

    almost got the timing mechanism I think

--- assets/helpers.js
+++ assets/helpers.js
@@ -1,3 +1,36 @@
 window.onload = () => {
-  console.log('start here');
+  return;
 };
+class runningTimer {
+  constructor() {
+    this.startTime = new Date();
+  }
+  getTime() {
+    return new Date();
+  }
+}
+function startTimer() {
+  const qualifyMinutes = getElement('qualify');
+  const limitMinutes = getElement('limit');
+  if (!qualifyMinutes) { alert('Input qualifing time'); return; }
+  if (!limitMinutes) { alert('Input time limit'); return; }
+  const timer = new runningTimer();
+  getElement('whiteCard').style.display = 'block';
+  getElement('selection').style.display = 'none';
+  console.log('start');
+  updateOnScreenTimer();
+  function updateOnScreenTimer() {
+    getElement('currentTime').innerHTML = calcTime();
+    console.log('test');
+    setTimeout(updateOnScreenTimer, 1000);
+  }
+  function calcTime() {
+    return getSeconds();
+    function getSeconds() {
+      return (new Date().getSeconds() - test.getSeconds() + 60) % 60;
+    }
+  }
+}
+function getElement(id) {
+  return document.getElementById(id);
+}
